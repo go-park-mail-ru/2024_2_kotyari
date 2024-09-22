a9332ebda3c8c1dd0ea78e13388aabd0e85b875d
Gleb Karpikhin <glebkarpikhin@gmail.com>
2024-09-22 16:27:18 +0300

    feat: Добавлен тест на пустой параметр

--- api/signup_test.go
+++ api/signup_test.go
@@ -84,6 +84,25 @@ func TestSignupHandler(t *testing.T) {
 		}
 	})
 
+	t.Run("Test for empty params password format", func(t *testing.T) {
+		req := httptest.NewRequest("POST", "/signup",
+			strings.NewReader(`{"username":"","email":"test@test.com", "password":"testtest"}`))
+		rr := httptest.NewRecorder()
+		SignupHandler(rr, req)
+
+		res := rr.Result()
+		var httpError HTTPErrorResponse
+		err := json.NewDecoder(res.Body).Decode(&httpError)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if httpError.ErrorCode != http.StatusBadRequest || httpError.ErrorMessage != ErrEmptyRequestParams.Error() {
+			t.Errorf("Expected error code %v , error message: %s, got error code: %v, error message: %s",
+				http.StatusBadRequest, ErrEmptyRequestParams.Error(), httpError.ErrorCode, httpError.ErrorMessage)
+		}
+	})
+
 	t.Run("Test for OK", func(t *testing.T) {
 		req := httptest.NewRequest("POST", "/signup",
 			strings.NewReader(`{"username":"testing","email":"test@test.com", "password":"testtest"}`))
