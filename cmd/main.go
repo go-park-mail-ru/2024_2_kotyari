6cf55a93972a40bfb271f441f8c17fc256b831d5
KurenkovDmitry <67153890+KurenkovDmitry@users.noreply.github.com>
2024-09-26 09:09:53 +0300

    [feat] Написать бекэнд для страницы каталог товаров
    беккенд для страницы каталог товаров

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,35 @@ import (
 	"log"
 	"net/http"
 
+	httpSwagger "github.com/swaggo/http-swagger"
+
 	"2024_2_kotyari/config"
 	"2024_2_kotyari/handlers"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+// @title Product Catalog API
+// @version 1.0
+// @description API для получения продуктов из каталога.
+// @host localhost:8000
+// @BasePath /catalog
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file", err.Error())
+		return
+	}
+
+	config.Init()
 	r := mux.NewRouter()
 
-	r.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
-	r.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
-	r.HandleFunc("/protected", handlers.ProtectedHandler).Methods("GET")
+	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
-	fmt.Println("Сервер запущен на", config.ServerAddress)
-	log.Fatal(http.ListenAndServe(config.ServerAddress, r))
+	r.HandleFunc("/catalog/products", handlers.ProductsHandler).Methods("GET")
+	r.HandleFunc("/catalog/product/{id}", handlers.ProductByIDHandler).Methods("GET")
+
+	fmt.Println("Сервер запущен на", config.GetServerAddress())
+	log.Fatal(http.ListenAndServe(config.GetServerAddress(), r))
 }
---
+++ handlers/cards.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"2024_2_kotyari/db"
+
+	"github.com/gorilla/mux"
+)
+
+// @Summary Get Products
+// @Description Возвращает список всех продуктов
+// @Tags Products
+// @Produce json
+// @Success 200 {object} map[string]db.Product
+// @Failure 500 {string} string "Ошибка при кодировании JSON"
+// @Router /catalog/products [get]
+func ProductsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	products := db.GetAllProducts()
+
+	err := json.NewEncoder(w).Encode(products)
+	if err != nil {
+		http.Error(w, "Ошибка при кодировании JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// @Summary Get Product by ID
+// @Description Возвращает продукт по его ID
+// @Tags Products
+// @Produce json
+// @Param id path string true "Product ID"
+// @Success 200 {object} db.Product
+// @Failure 404 {string} string "Продукт не найден"
+// @Failure 500 {string} string "Ошибка при кодировании JSON"
+// @Router /catalog/product/{id} [get]
+func ProductByIDHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	product, exists := db.GetProductByID(productID)
+	if !exists {
+		http.Error(w, "Продукт не найден", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(product)
+	if err != nil {
+		http.Error(w, "Ошибка при кодировании JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
