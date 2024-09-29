f1853c1541a5957a432fc8582e41d077e312a151
goriiin <afk00.17@bk.ru>
2024-09-30 00:29:47 +0300

    [fix]

--- internal/db/data.go
+++ internal/db/data.go
@@ -354,6 +354,7 @@ var productsData = map[string]Product{
 		ShortDescription: "Насадки для электрической зубной щетки Philips Sonicare",
 		URL:              "/catalog/product/40",
 		Currency:         "Р",
+	},
 }
 
 var usersData = map[string]User{
