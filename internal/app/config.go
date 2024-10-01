63c8f49bfd11c07f0514b99105aabf0a17350d67
KurenkovDmitry <67153890+KurenkovDmitry@users.noreply.github.com>
2024-10-01 03:50:45 +0300

    [improve] новый cors

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,8 +1,8 @@
 package app
 
 import (
-	"github.com/rs/cors"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -11,32 +11,37 @@ const (
 	hour   = 60 * minute
 )
 
-func setUpCORS() *cors.Cors {
-	return cors.New(cors.Options{
-		AllowedOrigins: []string{
-			"http://localhost:3000",
-			"http://localhost:8080",
-			"http://127.0.0.1:3000",
-			"http://127.0.0.1:8080",
-			"94.139.246.241",
-			"http://94.139.246.241:8000",
-			"http://94.139.246.241",
-		},
-		AllowedMethods: []string{
-			http.MethodGet,
-			http.MethodPost,
-			http.MethodPut,
-			http.MethodDelete,
-			http.MethodOptions},
-		AllowedHeaders: []string{
-			"Accept",
-			"Accept-Language",
-			"Content-Type",
-			"Authorization",
-			"Access-Control-Allow-Origin",
-		},
-		AllowCredentials: true,
-		MaxAge:           hour,
-		Debug:            false,
+func setupCORS(w http.ResponseWriter, req *http.Request) {
+	allowedOrigins := map[string]bool{
+		"http://localhost:3000":      true,
+		"http://localhost:8080":      true,
+		"http://127.0.0.1:3000":      true,
+		"http://127.0.0.1:8080":      true,
+		"94.139.246.241":             true,
+		"http://94.139.246.241:8000": true,
+		"http://94.139.246.241":      true,
+	}
+
+	if origin := req.Header.Get("Origin"); allowedOrigins[origin] {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+	}
+
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, Authorization, Access-Control-Allow-Origin")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Max-Age", strconv.Itoa(hour))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		setupCORS(w, req)
+
+		// Если метод OPTIONS, то возвращаем пустой ответ с нужными заголовками
+		if req.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, req)
 	})
 }
--- internal/app/http_server.go
+++ internal/app/http_server.go
@@ -1,13 +1,14 @@
 package app
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/config"
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/db"
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/handlers"
 	"github.com/gorilla/mux"
 	httpSwagger "github.com/swaggo/http-swagger"
-	"log"
-	"net/http"
 )
 
 type Server struct {
@@ -26,7 +27,7 @@ func NewServer() *Server {
 	}
 }
 
-func (s *Server) Run() {
+func (s *Server) setupRoutes() {
 	s.r.HandleFunc("/login", s.auth.Login).Methods(http.MethodPost)
 	s.r.HandleFunc("/logout", s.auth.Logout).Methods(http.MethodPost)
 	s.r.HandleFunc("/signup", s.auth.SignUp).Methods(http.MethodPost)
@@ -39,8 +40,13 @@ func (s *Server) Run() {
 	s.r.HandleFunc("/favorite", s.auth.IsLogin).Methods(http.MethodGet)
 	s.r.HandleFunc("/account", s.auth.IsLogin).Methods(http.MethodGet)
 	s.r.HandleFunc("/", s.auth.IsLogin).Methods(http.MethodGet)
+}
 
-	handler := setUpCORS().Handler(s.r)
+func (s *Server) Run() {
+	s.setupRoutes()
+
+	// Оборачиваем маршруты CORS middleware
+	handler := corsMiddleware(s.r)
 
 	log.Printf("Сервер запущен на: %s\n", s.cfg.ServerAddress)
 	err := http.ListenAndServe(s.cfg.ServerAddress, handler)
