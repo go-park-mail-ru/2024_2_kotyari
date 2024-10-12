cd1eb8bac31356a32d44e13fda8129a2a966e200
KurenkovDmitry <67153890+KurenkovDmitry@users.noreply.github.com>
2024-10-13 00:30:10 +0300

    feat: поправлены несколько полей и удалена лишняя таблица, добавлена erd

--- assets/init.sql
+++ assets/init.sql
@@ -6,10 +6,9 @@ CREATE TABLE IF NOT EXISTS "user" (
 	"age" smallint CHECK (age >= 0 AND age <= 120),
 	"avatar_url" text,
 	"password" text NOT NULL,
-	"salt" text NOT NULL,
 	"active" boolean NOT NULL DEFAULT true,
 	"blocked" boolean NOT NULL DEFAULT false,
-	"time_to_block" integer NOT NULL DEFAULT '-1' CHECK (time_to_block >= -1),
+	"block_until" timestamp with time zone DEFAULT NULL,
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id")
@@ -37,12 +36,12 @@ CREATE TABLE IF NOT EXISTS "product" (
 -- Таблица избранных продуктов для пользователей
 CREATE TABLE IF NOT EXISTS "favorite" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"user" bigint NOT NULL,
+	"user_id" bigint NOT NULL,
 	"product_id" bigint NOT NULL,
 	"is_favorite" boolean NOT NULL DEFAULT true,  -- Флаг, указывающий, что продукт в избранном
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
-	FOREIGN KEY ("user") REFERENCES "user"("id"),
+	FOREIGN KEY ("user_id") REFERENCES "user"("id"),
 	FOREIGN KEY ("product_id") REFERENCES "product"("id")
 );
 
@@ -70,39 +69,56 @@ CREATE TABLE IF NOT EXISTS "stock_address" (
 -- Таблица заказов, связанная с пользователями и складскими адресами
 CREATE TABLE IF NOT EXISTS "order" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"user" bigint NOT NULL,
+	"user_id" bigint NOT NULL,
 	"address" text NOT NULL DEFAULT 'NONE',  -- Адрес доставки (если не используется стоковый адрес)
 	"stock_address_id" bigint,  -- Ссылка на таблицу стоковых адресов
 	"price" integer NOT NULL,
 	"status" text NOT NULL DEFAULT 'paid',
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
-	FOREIGN KEY ("user") REFERENCES "user"("id"),
+	FOREIGN KEY ("user_id") REFERENCES "user"("id"),
 	FOREIGN KEY ("stock_address_id") REFERENCES "stock_address"("id")  -- Внешний ключ для стоковых адресов
+);
+
+-- Таблица для опций продукта, таких как цвет или размер, с возможностью указать доступные значения
+CREATE TABLE IF NOT EXISTS "product_option" (
+	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"product_id" bigint NOT NULL,  -- Ссылка на товар
+	"option_name" text NOT NULL,  -- Название опции (например, цвет, размер)
+	"option_values" jsonb NOT NULL,  -- Возможные значения опции в формате JSON (например, ["красный", "синий", "зеленый"])
+	"active" boolean NOT NULL DEFAULT true,
+	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ("id"),
+	FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE
 );
 
 -- Таблица связывает продукты с заказами, хранит количество и дату доставки продукта в заказе
 CREATE TABLE IF NOT EXISTS "product_and_order" (
 	"order_id" bigint NOT NULL,
 	"product_id" bigint NOT NULL,
+    "option_id" bigint NOT NULL,  -- Ссылка на опцию
+	"option_value" text NOT NULL,  -- Конкретное выбранное значение опции (например, красный или XL)
 	"count" integer NOT NULL DEFAULT '1' CHECK (count > 0),
 	"delivery_date" timestamp with time zone NOT NULL,  -- Дата доставки продукта
-	PRIMARY KEY ("order_id", "product_id"),
+	PRIMARY KEY ("order_id", "product_id", "option_id"),
 	FOREIGN KEY ("order_id") REFERENCES "order"("id"),
-	FOREIGN KEY ("product_id") REFERENCES "product"("id")
+	FOREIGN KEY ("product_id") REFERENCES "product"("id"),
+    FOREIGN KEY ("option_id") REFERENCES "product_option"("id")
 );
 
 -- Таблица корзины пользователя, где хранится информация о продуктах в корзине
 CREATE TABLE IF NOT EXISTS "cart" (
-	"user" bigint NOT NULL,
+	"user_id" bigint NOT NULL,
 	"product_id" bigint NOT NULL,
+    "option_id" bigint NOT NULL,  -- Ссылка на опцию
 	"count" integer NOT NULL CHECK (count > 0),
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"delivery_date" timestamp with time zone,  -- Дата доставки для товара в корзине
-	PRIMARY KEY ("user", "product_id"),  -- Комбинированный первичный ключ, чтобы каждая пара была уникальной
-	FOREIGN KEY ("user") REFERENCES "user"("id"),
-	FOREIGN KEY ("product_id") REFERENCES "product"("id")
+	PRIMARY KEY ("user_id", "product_id", "option_id"),  -- Комбинированный первичный ключ, чтобы каждая пара была уникальной
+	FOREIGN KEY ("user_id") REFERENCES "user"("id"),
+	FOREIGN KEY ("product_id") REFERENCES "product"("id"),
+    FOREIGN KEY ("option_id") REFERENCES "product_option"("id")
 );
 
 -- Таблица тегов для продуктов
@@ -118,12 +134,12 @@ CREATE TABLE IF NOT EXISTS "tag" (
 -- Таблица, связывающая продукты и теги
 CREATE TABLE IF NOT EXISTS "product_tag" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"product" bigint NOT NULL,
-	"tag" bigint NOT NULL,
+	"product_id" bigint NOT NULL,
+	"tag_id" bigint NOT NULL,
 	"active" boolean NOT NULL DEFAULT true,
 	PRIMARY KEY ("id"),
-	FOREIGN KEY ("product") REFERENCES "product"("id"),
-	FOREIGN KEY ("tag") REFERENCES "tag"("id")
+	FOREIGN KEY ("product_id") REFERENCES "product"("id"),
+	FOREIGN KEY ("tag_id") REFERENCES "tag"("id")
 );
 
 -- Таблица продавцов, с указанием типа продавца (физическое лицо или компания), проверенности и рейтинга
@@ -145,32 +161,8 @@ ALTER TABLE "product" ADD FOREIGN KEY ("seller_id") REFERENCES "seller"("id") ON
 -- Индекс для ускорения поиска товаров по продавцу
 CREATE INDEX idx_product_seller ON product(seller_id);
 
--- Таблица для опций продукта, таких как цвет или размер, с возможностью указать доступные значения
-CREATE TABLE IF NOT EXISTS "product_option" (
-	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"product_id" bigint NOT NULL,  -- Ссылка на товар
-	"option_name" text NOT NULL,  -- Название опции (например, цвет, размер)
-	"option_values" jsonb NOT NULL,  -- Возможные значения опции в формате JSON (например, ["красный", "синий", "зеленый"])
-	"active" boolean NOT NULL DEFAULT true,
-	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY ("id"),
-	FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE
-);
-
 -- Индекс для ускорения поиска товаров по опциям
 CREATE INDEX idx_product_option_product ON product_option(product_id);
-
--- Таблица для хранения выбранных значений опций для конкретного продукта в заказе
-CREATE TABLE IF NOT EXISTS "order_product_option" (
-	"order_id" bigint NOT NULL,
-	"product_id" bigint NOT NULL,
-	"option_id" bigint NOT NULL,  -- Ссылка на опцию
-	"option_value" text NOT NULL,  -- Конкретное выбранное значение опции (например, красный или XL)
-	PRIMARY KEY ("order_id", "product_id", "option_id"),
-	FOREIGN KEY ("order_id") REFERENCES "order"("id"),
-	FOREIGN KEY ("product_id") REFERENCES "product"("id"),
-	FOREIGN KEY ("option_id") REFERENCES "product_option"("id")
-);
 
 -- Индексы для обеспечения быстрого поиска
 CREATE INDEX idx_product_created_at ON product(created_at);
@@ -203,5 +195,10 @@ EXECUTE FUNCTION update_updated_at_column();
 
 CREATE TRIGGER set_updated_at_cart
 BEFORE UPDATE ON "cart"
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+CREATE TRIGGER set_updated_at_seller
+BEFORE UPDATE ON "seller"
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
