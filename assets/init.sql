73f6afa96b6024ff78d2f560d0f73c684a0951e4
Gleb Karpikhin <glebkarpikhin@gmail.com>
2024-10-18 17:35:55 +0300

    fix: Поправлены некоторые моменты в DB

--- assets/init.sql
+++ assets/init.sql
@@ -1,7 +1,7 @@
 -- Таблица пользователей, содержащая данные о зарегистрированных пользователях (почта, логин, пароль, аватар и др.)
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-	"email" text NOT NULL,
+	"email" text UNIQUE NOT NULL,
 	"username" text NOT NULL,
 	"city" text NOT NULL DEFAULT 'Москва',
 	"age" smallint CHECK (age >= 0 AND age <= 120),
@@ -17,11 +17,12 @@ CREATE TABLE IF NOT EXISTS "users" (
 -- Таблица продуктов, хранящая информацию о товаре (цена, описание, скидка, изображения, характеристики и др.)
 CREATE TABLE IF NOT EXISTS "products" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
+    "seller_id" bigint NOT NULL,
 	"count" integer NOT NULL DEFAULT '1' CHECK (count >= 0),
-	"price" integer NOT NULL CHECK (price >= 0),  -- Новая цена
-	"old_price" integer CHECK (old_price >= 0),  -- Старая цена
+	"price" integer NOT NULL CHECK (price > 0),  -- Новая цена
+	"old_price" integer CHECK (old_price > 0),  -- Старая цена
 	"currency" text NOT NULL DEFAULT 'rub',
-	"discount" smallint CHECK (discount >= 0 AND discount <= 100),  -- Скидка
+	"discount" smallint CHECK (discount >= 0 AND discount < 100),  -- Скидка
 	"description" text,
 	"short_description" text NOT NULL,
 	"rating" real DEFAULT 0 NOT NULL CHECK (rating >= 0 AND rating <= 5),
@@ -31,7 +32,8 @@ CREATE TABLE IF NOT EXISTS "products" (
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"characteristics" jsonb,  -- Характеристики продукта в формате JSON
-	PRIMARY KEY ("id")
+	PRIMARY KEY ("id"),
+    FOREIGN KEY ("seller_id") REFERENCES "sellers"("id") ON DELETE CASCADE
 );
 
 -- Таблица избранных продуктов для пользователей
@@ -39,11 +41,11 @@ CREATE TABLE IF NOT EXISTS "favorites" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"user_id" bigint NOT NULL,
 	"product_id" bigint NOT NULL,
-	"is_favorite" boolean NOT NULL DEFAULT true,  -- Флаг, указывающий, что продукт в избранном
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("user_id") REFERENCES "users"("id"),
-	FOREIGN KEY ("product_id") REFERENCES "products"("id")
+	FOREIGN KEY ("product_id") REFERENCES "products"("id"),
+    UNIQUE ("user_id", "product_id")
 );
 
 -- Таблица для хранения изображений продукта
@@ -51,7 +53,6 @@ CREATE TABLE IF NOT EXISTS "product_images" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"product_id" bigint NOT NULL,
 	"image_url" text NOT NULL,
-	"active" boolean NOT NULL DEFAULT true,
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE
@@ -67,14 +68,16 @@ CREATE TABLE IF NOT EXISTS "stock_address" (
 	PRIMARY KEY ("id")
 );
 
+
+CREATE TYPE order_status AS ENUM ('awaiting_payment', 'paid', 'delivered', 'cancelled');
 -- Таблица заказов, связанная с пользователями и складскими адресами
 CREATE TABLE IF NOT EXISTS "orders" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"user_id" bigint NOT NULL,
-	"address" text NOT NULL DEFAULT 'NONE',  -- Адрес доставки (если не используется стоковый адрес)
+	"address" text NOT NULL DEFAULT NULL,  -- Адрес доставки (если не используется стоковый адрес)
 	"stock_address_id" bigint,  -- Ссылка на таблицу стоковых адресов
-	"total_price" integer NOT NULL CHECK ("total_price" >= 0),
-	"status" text NOT NULL DEFAULT 'paid',
+	"total_price" integer NOT NULL CHECK ("total_price" > 0),
+	"status" order_status DEFAULT 'paid',
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
@@ -87,14 +90,24 @@ CREATE TABLE IF NOT EXISTS "product_options" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"product_id" bigint NOT NULL,  -- Ссылка на товар
 	"option_name" text NOT NULL,  -- Название опции (например, цвет, размер)
-	"option_values" jsonb NOT NULL,  -- Возможные значения опции в формате JSON (например, ["красный", "синий", "зеленый"])
-	"count" integer NOT NULL DEFAULT '1' CHECK (count >= 0),
-	"price" integer NOT NULL CHECK (price >= 0),  -- Новая цена
-	"old_price" integer CHECK (old_price >= 0),  -- Старая цена
+	"option_values" jsonb NOT NULL,  -- Возможные значения опции в формате JSON
+    --{
+    --    "options": [
+    --        {
+    --            "value": "красный",
+    --            "price": 1000,
+    --            "count": 10
+    --            // old price, discount, etc
+    --        },
+    --        {
+    --             "value": "синий",
+    --             "price": 1100,
+    --             "count": 5
+    --         }
+    --    ]
+    --}
 	"currency" text NOT NULL DEFAULT 'rub',
-	"discount" smallint CHECK (discount >= 0 AND discount <= 100),  -- Скидка
 	"image_url" text,
-	"active" boolean NOT NULL DEFAULT true,
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE
@@ -105,7 +118,6 @@ CREATE TABLE IF NOT EXISTS "product_option_images" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"option_id" bigint NOT NULL,
 	"image_url" text NOT NULL,
-	"active" boolean NOT NULL DEFAULT true,
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("option_id") REFERENCES "product_options"("id") ON DELETE CASCADE
@@ -135,7 +147,6 @@ CREATE TABLE IF NOT EXISTS "carts" (
     "option_id" bigint,  -- Ссылка на опцию
 	"count" integer NOT NULL CHECK (count > 0),
 	"is_selected" boolean DEFAULT false,
-	"active" boolean NOT NULL DEFAULT true,
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"updated_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"delivery_date" timestamp with time zone,  -- Дата доставки для товара в корзине
@@ -153,7 +164,7 @@ CREATE TABLE IF NOT EXISTS "categories" (
 	"name" text NOT NULL,
 	"resultant_limitation" smallint NOT NULL DEFAULT '0' CHECK (resultant_limitation >= 0 AND resultant_limitation <= 120),
 	"active" boolean NOT NULL DEFAULT true,
-	"color" text DEFAULT 'rgb(160, 60, 236)',
+	"color" text DEFAULT 'oklch(58.26% 0.2484 305.7)',
 	PRIMARY KEY ("id")
 );
 
@@ -168,11 +179,12 @@ CREATE TABLE IF NOT EXISTS "product_categories" (
 	FOREIGN KEY ("category_id") REFERENCES "categories"("id")
 );
 
+CREATE TYPE seller_type AS ENUM ('individual', 'company');
 -- Таблица продавцов, с указанием типа продавца (физическое лицо или компания), проверенности и рейтинга
 CREATE TABLE IF NOT EXISTS "sellers" (
 	"id" bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
 	"seller_name" text NOT NULL,  -- Название продавца (для компаний) или ФИО (для физических лиц)
-	"seller_type" text NOT NULL CHECK (seller_type IN ('individual', 'company')),  -- Тип продавца (физ лицо или компания)
+	"seller_type" seller_type NOT NULL CHECK (seller_type IN ('individual', 'company')),  -- Тип продавца (физ лицо или компания)
 	"verified" boolean NOT NULL DEFAULT false,  -- Флаг, указывающий, что продавец проверен
 	"rating" real CHECK (rating >= 0 AND rating <= 5) DEFAULT 0 NOT NULL,  -- Рейтинг продавца от 0 до 5
 	"created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -195,37 +207,6 @@ CREATE INDEX idx_product_created_at ON products(created_at);
 CREATE INDEX idx_product_price ON products(price);
 CREATE INDEX idx_product_characteristics ON products USING gin (characteristics);
 
-
--- Функция для автоматического обновления поля updated_at при изменении записи
-CREATE OR REPLACE FUNCTION update_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-	NEW.updated_at = CURRENT_TIMESTAMP;
-	RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER set_updated_at_user
-BEFORE UPDATE ON "users"
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER set_updated_at_product
-BEFORE UPDATE ON "products"
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER set_updated_at_cart
-BEFORE UPDATE ON "carts"
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER set_updated_at_seller
-BEFORE UPDATE ON "sellers"
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
-
-CREATE TRIGGER set_updated_at_seller
-BEFORE UPDATE ON "orders"
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
+-- Не уверен насколько это нужно
+ALTER TABLE orders
+ADD CONSTRAINT status_check CHECK (status IN ('awaiting_payment', 'paid', 'delivered', 'cancelled'));
