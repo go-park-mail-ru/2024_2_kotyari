3680eaf2f0795eb0eebdb1b6e11fedb6590d33aa
jojosoderqvist <glebkarpikhin@gmail.com>
2024-10-24 02:30:33 +0300

    fix: По комментариям

--- internal/usecase/user/get_user_by_email.go
+++ internal/usecase/user/get_user_by_email.go
@@ -2,13 +2,14 @@ package user
 
 import (
 	"context"
+
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/errs"
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/model"
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/utils"
 )
 
 func (us *UsersService) GetUserByEmail(ctx context.Context, user model.User) (string, error) {
-	dbUser, err := us.userManager.GetUserByEmail(ctx, user)
+	dbUser, err := us.userRepo.GetUserByEmail(ctx, user)
 	if err != nil {
 		return "", err
 	}
