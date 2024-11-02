9de1ad7d025434b65a5848d179beec9f93b819bb
Gleb Karpikhin <glebkarpikhin@gmail.com>
2024-11-02 22:08:37 +0300

    fix: Правильно работает добавление в корзину

--- internal/repository/cart/add_product.go
+++ internal/repository/cart/add_product.go
@@ -3,9 +3,22 @@ package cart
 import (
 	"context"
 	"log/slog"
+
+	"github.com/go-park-mail-ru/2024_2_kotyari/internal/errs"
+	"github.com/jackc/pgx/v5"
 )
 
 func (cs *CartsStore) AddProduct(ctx context.Context, productID uint32, userID uint32) error {
+	tx, err := cs.db.BeginTx(ctx, pgx.TxOptions{
+		AccessMode: pgx.ReadWrite,
+	})
+	if err != nil {
+		cs.log.Error("[CartStore.RemoveCartProduct] failed to start transaction", "error", slog.String("error", err.Error()))
+
+		return errs.InternalServerError
+
+	}
+
 	const query = `
 		insert into carts(user_id, product_id, count, is_selected, is_deleted)
 		values ($1, $2, 1, true, false)
@@ -23,6 +36,27 @@ func (cs *CartsStore) AddProduct(ctx context.Context, productID uint32, userID u
 
 		return err
 	}
+
+	err = cs.updateProductCount(ctx, productID, 1)
+	if err != nil {
+		cs.log.Error("[CartStore.ChangeProductCount] Error occurred when changing cart product count", slog.String("error", err.Error()))
+
+		return err
+	}
+
+	defer func() {
+		if err != nil {
+			cs.log.Error("[CartStore.ChangeProductCount] an error occurred", slog.String("error", err.Error()))
+
+			err = tx.Rollback(ctx)
+			cs.log.Error("[CartStore.ChangeProductCount] Transaction error occurred", slog.String("error", err.Error()))
+		}
+
+		err = tx.Commit(ctx)
+		if err != nil {
+			cs.log.Error("[CartStore.ChangeProductCount] Transaction error occurred", slog.String("error", err.Error()))
+		}
+	}()
 
 	return nil
 }
--- internal/usecase/cart/add_product.go
+++ internal/usecase/cart/add_product.go
@@ -6,13 +6,12 @@ import (
 	"log/slog"
 
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/errs"
-	"github.com/jackc/pgx/v5"
 )
 
 func (cm *CartManager) AddProduct(ctx context.Context, productID uint32, userID uint32) error {
 	product, err := cm.cartRepository.GetCartProduct(ctx, productID, userID)
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
+		if errors.Is(err, errs.ProductNotInCart) {
 			err = cm.cartRepository.AddProduct(ctx, productID, userID)
 			if err != nil {
 				cm.log.Error("[CartManager.AddProduct] Error adding product", slog.String("error", err.Error()))
