e912a7c452f7b68d12ab77f7fa83583c68f76f4c
goriiin <afk00.17@bk.ru>
2024-11-06 04:19:52 +0300

    check

--- internal/repository/file/get_file.go
+++ internal/repository/file/get_file.go
@@ -2,22 +2,15 @@ package file
 
 import (
 	"fmt"
-	"log"
-	"log/slog"
 	"os"
 )
 
 func (repo *FilesRepo) GetFile(filename string) (*os.File, error) {
-	log.Printf(" [ FilesRepo.GetFile ] Зашли")
-
 	fullPath, err := repo.buildPath(filename)
 	if err != nil {
 
-		log.Printf("[ FilesRepo.GetFile ] ошибка %s", err.Error())
 		return nil, err
 	}
-
-	log.Printf(" [ FilesRepo.GetFile ] Getting file %s", fullPath)
 
 	file, err := os.Open(fullPath)
 	if err != nil {
@@ -28,6 +21,5 @@ func (repo *FilesRepo) GetFile(filename string) (*os.File, error) {
 		return nil, fmt.Errorf("ошибка при открытии файла: %w", err)
 	}
 
-	repo.log.Debug("Файл успешно открыт для чтения", slog.String("path", fullPath))
 	return file, nil
 }
