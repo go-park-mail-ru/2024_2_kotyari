2181068234459475cff592bd6de2157591163714
jojosoderqvist <glebkarpikhin@gmail.com>
2024-11-13 00:38:48 +0300

    feat: Начал делать поиск

---
+++ internal/repository/search/search_title_suggestions.go
@@ -0,0 +1,13 @@
+package search
+
+import (
+	"context"
+
+	"github.com/go-park-mail-ru/2024_2_kotyari/internal/model"
+)
+
+func (s *SearchStore) GetSearchTitleSuggestions(ctx context.Context) model.SearchTitleSuggestion {
+	const query = `
+		
+	`
+}
