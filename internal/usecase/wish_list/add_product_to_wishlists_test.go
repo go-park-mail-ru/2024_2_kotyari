fee1ee7a8f4c16e857a0ced45d383f86fac2bb1d
goriiin <dkoshenkofff@mail.ru>
2024-12-17 21:05:50 +0300

    init

---
+++ internal/usecase/wish_list/add_product_to_wishlists_test.go
@@ -0,0 +1,13 @@
+package wish_list
+
+import "testing"
+
+func TestWishListUsecase_AddProductToWishlists(t *testing.T) {
+	t.Parallel()
+
+	type want error
+
+	tests := []struct {
+		name string
+	}
+}
