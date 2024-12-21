c65ce5560c97e91b984a47a45e07a78057beeb47
goriiin <dkoshenkofff@mail.ru>
2024-12-22 02:47:43 +0300

    [fix] ошибка 404 при пустой корзине

--- internal/usecase/orders/create_order_from_cart.go
+++ internal/usecase/orders/create_order_from_cart.go
@@ -2,9 +2,8 @@ package orders
 
 import (
 	"context"
-	"errors"
+	"github.com/go-park-mail-ru/2024_2_kotyari/internal/errs"
 	"log/slog"
-	"strconv"
 	"time"
 
 	order "github.com/go-park-mail-ru/2024_2_kotyari/internal/model"
@@ -32,7 +31,7 @@ func (m *OrdersManager) CreateOrderFromCart(ctx context.Context, address string,
 
 	if len(cartItems) == 0 {
 		m.logger.Error("[OrdersManager.CreateOrderFromCart] cart is empty for user: ", slog.Uint64("user_id", uint64(userID)))
-		return nil, errors.New("cart is empty for user: " + strconv.Itoa(int(userID)))
+		return nil, errs.EmptyCart
 	}
 
 	var totalPrice uint32
