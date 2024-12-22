3b2b48a1f7a56f08f9c76668eee897b21053d8c5
goriiin <dkoshenkofff@mail.ru>
2024-12-22 04:28:12 +0300

    [fix]

--- internal/repository/orders/create_order_from_cart.go
+++ internal/repository/orders/create_order_from_cart.go
@@ -8,34 +8,15 @@ import (
 	order "github.com/go-park-mail-ru/2024_2_kotyari/internal/model"
 	"github.com/go-park-mail-ru/2024_2_kotyari/internal/utils"
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
 )
 
 const defaultStatus = "awaiting_payment"
 
 func (r *OrdersRepo) CreateOrderFromCart(ctx context.Context, orderData *order.OrderFromCart) (*order.Order, error) {
-	tx, err := r.db.BeginTx(ctx, pgx.TxOptions{
-		IsoLevel: pgx.Serializable,
-	})
-	if err != nil {
-		r.logger.Error("[OrdersRepo.CreateOrderFromCart] Failed to start transaction", slog.String("error", err.Error()))
-		return nil, err
-	}
-
-	requestID, err := utils.GetContextRequestID(ctx)
+	_, err := utils.GetContextRequestID(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	r.logger.Info("[OrdersRepo.CreateOrderFromCart] Started executing", slog.Any("request-id", requestID))
-
-	defer func() {
-		if p := recover(); p != nil || err != nil {
-			_ = tx.Rollback(ctx)
-		} else {
-			_ = tx.Commit(ctx)
-		}
-	}()
 
 	const createOrderQuery = `
 		INSERT INTO orders (id, user_id, total_price, address, created_at, updated_at)
@@ -44,7 +25,7 @@ func (r *OrdersRepo) CreateOrderFromCart(ctx context.Context, orderData *order.O
 	`
 
 	var createdAt time.Time
-	err = tx.QueryRow(ctx, createOrderQuery, orderData.OrderID, orderData.UserID, orderData.TotalPrice, orderData.Address).Scan(&orderData.CreatedAt)
+	err = r.db.QueryRow(ctx, createOrderQuery, orderData.OrderID, orderData.UserID, orderData.TotalPrice, orderData.Address).Scan(&orderData.CreatedAt)
 	if err != nil {
 		r.logger.Error("[OrdersRepo.CreateOrderFromCart] failed to insert order", slog.String("error", err.Error()), slog.Uint64("user_id", uint64(orderData.UserID)))
 		return nil, err
@@ -59,7 +40,7 @@ func (r *OrdersRepo) CreateOrderFromCart(ctx context.Context, orderData *order.O
 	for _, p := range orderData.Products {
 		productOrderID := uuid.New()
 
-		_, err := tx.Exec(ctx, insertProductQuery, productOrderID, orderData.OrderID, p.ID, p.OptionID, p.Count, orderData.DeliveryDate)
+		_, err := r.db.Exec(ctx, insertProductQuery, productOrderID, orderData.OrderID, p.ID, p.OptionID, p.Count, orderData.DeliveryDate)
 		if err != nil {
 			r.logger.Error("[OrdersRepo.CreateOrderFromCart] failed to insert product in order", slog.String("error", err.Error()), slog.Uint64("user_id", uint64(orderData.UserID)))
 			return nil, err
@@ -72,7 +53,7 @@ func (r *OrdersRepo) CreateOrderFromCart(ctx context.Context, orderData *order.O
 		WHERE user_id = $1 AND is_selected = true AND is_deleted = false;
 	`
 
-	_, err = tx.Exec(ctx, removeCartItemsQuery, orderData.UserID)
+	_, err = r.db.Exec(ctx, removeCartItemsQuery, orderData.UserID)
 	if err != nil {
 		r.logger.Error("[OrdersRepo.CreateOrderFromCart] failed to remove selected cart items", slog.String("error", err.Error()), slog.Uint64("user_id", uint64(orderData.UserID)))
 		return nil, err
